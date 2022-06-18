8723645f0e6d2bb579eb448ff660fb3a76a87abe
Ara James Hanissian, Jr <68933196+ara-h@users.noreply.github.com>
2022-06-18 14:28:35 -0500

    Update config.py

--- script/config.py
+++ script/config.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 
-
+# A database.ini file should be placed in this directory
 def config(filename="database.ini", section="postgresql"):
     parser = ConfigParser()
     parser.read(filename)
